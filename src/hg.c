3451a16c2c25a532a23994a20ff63e93e5dc1c97
Jan Vlcek <jan.vlcek@i.cz>
2014-06-20 11:31:57 +0200

    Solaris 10 + gcc port
    * check header <sys/wait.h> macro WEXITSTATUS()
    * Makefile.in: PREFIX = @prefix@

--- src/hg.c
+++ src/hg.c
@@ -7,12 +7,17 @@
  * (at your option) any later version.
  */
 
+#include "../config.h"
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
 #include <unistd.h>
 
 #if defined __BEOS__ && !defined __HAIKU__
